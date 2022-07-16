4ed88f8c07d4b6c7dfd9fc239083c31ef2c6e1b4
KymicL <108959762+KymicL@users.noreply.github.com>
2022-07-16 18:32:02 -0500

    Update CrazyHub.lua

--- CrazyHub.lua
+++ CrazyHub.lua
@@ -1,3 +1,7 @@
 if game.PlaceId == 9397663086 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KymicL/Scripts/main/main.lua"))()
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/KymicL/Scripts/main/main.lua"))()
+      elseif
+    
+   game.PlaceId == 8950974597 then
+ loadstring(game:HttpGet("https://raw.githubusercontent.com/KymicL/Scripts/main/And.lua"))()
 end
