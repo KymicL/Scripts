e13a6d6503d8e6e4878737c6127dbb77477ce608
KymicL <108959762+KymicL@users.noreply.github.com>
2022-07-15 20:24:44 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
+
+
+
+
+
+
+
+
+
+
 local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/shlexware/Orion/main/source")))()
 local Window = OrionLib:MakeWindow({Name = "Key System", HidePremium = false, SaveConfig = true, IntroEnabled = false})
 
